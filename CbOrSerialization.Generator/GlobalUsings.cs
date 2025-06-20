b928bf74cebd5919edfa43eadafb54d67315a1c8
Sunny Rajwadi <sunny@atcults.com>
2025-06-20 16:34:01 +0530

    Refactors generator project structure
    Updates the generator project by removing the direct project reference to the core library and instead uses analyzer mechanism.
    
    This change improves separation of concerns and simplifies the build process for the generator.
    
    Adds known naming policies enum to generator

--- CbOrSerialization.Generator/GlobalUsings.cs
+++ CbOrSerialization.Generator/GlobalUsings.cs
@@ -4,5 +4,4 @@ global using Microsoft.CodeAnalysis;
 global using Microsoft.CodeAnalysis.CSharp;
 global using Microsoft.CodeAnalysis.CSharp.Syntax;
 global using Microsoft.CodeAnalysis.Text;
-global using CbOrSerialization;
 
